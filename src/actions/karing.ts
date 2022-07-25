ba369b6c9c4f3c128f76cab93a43932bf8c4de8e
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2022-07-25 15:02:58 -0500

    change text with json

--- src/actions/karing.ts
+++ src/actions/karing.ts
@@ -34,6 +34,6 @@ export const getCarteraByTercero = async (tercero: number) => {
             },
         }
     )
-    const cartera = await response.text()
+    const cartera = await response.json()
     return cartera
 }
