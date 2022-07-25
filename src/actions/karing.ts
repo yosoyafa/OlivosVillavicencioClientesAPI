b6e93a558150f0e7195913638b51ed2c864bff10
Carlos Andrés Afanador Cabal <carlosafanador97@gmail.com>
2022-07-25 13:20:46 -0500

    add requestOptions

--- src/actions/karing.ts
+++ src/actions/karing.ts
@@ -1,18 +1,23 @@
 import fetch from 'node-fetch'
+import { RequestInit } from 'node-fetch'
 
 const tokenUrl = 'https://erp.karing.com.co/900679170/Api/LoginAPI/Authenticate/'
 const tercerosUrl = 'https://erp.karing.com.co/900679170/Api/DocumentosApi'
 
-export const getToken = () => fetch(tokenUrl, {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    usuario: 'CRM',
-    clave: 'CRMfunintegrales'
-})
+const requestOptions: RequestInit = {
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        usuario: 'CRM',
+        clave: 'CRMfunintegrales'
+    }
+}
+
+export const getToken = () => fetch(tokenUrl, requestOptions)
     .then((response) => response.json())
     .catch((err) => console.error(err))
 
-export const getCarteraByTercero = ({ token, tercero }: { token: string, tercero: number }) => {
+/* export const getCarteraByTercero = ({ token, tercero }: { token: string, tercero: number }) => {
     axios.get(
         tercerosUrl + `/GetCarteraTerceroV2?tercero=${tercero}`,
         {
@@ -23,4 +28,4 @@ export const getCarteraByTercero = ({ token, tercero }: { token: string, tercero
             }
         }
     ).then((response) => response.data)
-}
+} */
